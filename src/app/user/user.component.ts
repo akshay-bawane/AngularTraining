04fbfea60e8707f38f83be5650361d929c65ec13
Akshay <akshaybawane93@gmail.com>
2024-09-03 14:27:15 +0530

    Using objects

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,17 +7,23 @@ import { Component, EventEmitter, Input, Output, output } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Input({required: true}) user!: {
+    id: string,
+    avatar: string,
+    name: string
+  };
 
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
