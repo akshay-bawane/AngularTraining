daea7edb9dee6dbb16a1a0c8e69dc813a46e5bb3
Akshay <akshaybawane93@gmail.com>
2024-09-03 19:53:51 +0530

    Adding new Task and Cancel

---
+++ src/app/tasks/add-task/add-task.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css'
+})
+export class AddTaskComponent {
+  @Output() cancel = new EventEmitter<boolean>();
+
+  onCancelTask(){
+    this.cancel.emit(false);
+  }
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { AddTaskComponent } from "./add-task/add-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent]
+  imports: [TaskComponent, AddTaskComponent]
 })
 export class TasksComponent {
   @Input({required:true}) userId!: string;
   @Input({required: true}) name!: string;
+
+  isAddingTask: boolean = false;
 
   tasks = [
     {
@@ -45,4 +48,13 @@ export class TasksComponent {
   onCompleteTask(id: string){
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelTask(){
+    this.isAddingTask = false;
+  }
+
 }
