c26183b8bb539c553fef9bbb9ee57bca9cb6570a
Akshay <akshaybawane93@gmail.com>
2024-09-03 19:10:09 +0530

    Complete task for clearing the task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -41,4 +41,8 @@ export class TasksComponent {
   get selectedUserTasks(){
     return this.tasks.filter((task) => task.userId === this.userId)
   }
+
+  onCompleteTask(id: string){
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
