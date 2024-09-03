0dc3a69a54acbc50e101ca34782134f03f3051cb
Akshay <akshaybawane93@gmail.com>
2024-09-03 23:30:32 +0530

    Services complete

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-add-task',
@@ -10,22 +10,26 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
-  @Output() cancel = new EventEmitter<boolean>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<boolean>();
 
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
+  private tasksService = inject(TasksService)
 
   onCancelTask() {
-    this.cancel.emit(false);
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    });
+    },
+      this.userId
+    );
+    this.close.emit();
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { AddTaskComponent } from "./add-task/add-task.component";
-import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service'
 
 @Component({
   selector: 'app-tasks',
@@ -11,61 +11,22 @@ import { type NewTaskData } from './task/task.model';
   imports: [TaskComponent, AddTaskComponent]
 })
 export class TasksComponent {
-  @Input({required:true}) userId!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
 
   isAddingTask: boolean = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    }
-  ];
+  constructor(private tasksService: TasksService) { }
 
-  get selectedUserTasks(){
-    return this.tasks.filter((task) => task.userId === this.userId)
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string){
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
-
-  onStartAddTask(){
+  onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelTask(){
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-      userId: this.userId
-    });
+  onCloseTask() {
     this.isAddingTask = false;
   }
 
