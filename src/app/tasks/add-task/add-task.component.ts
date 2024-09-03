e40e65533fbb1c07d563e0635213d3f74939d9cf
Akshay <akshaybawane93@gmail.com>
2024-09-03 20:09:01 +0530

    Two way binding with Signal

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -11,11 +11,11 @@ import { FormsModule } from '@angular/forms';
 export class AddTaskComponent {
   @Output() cancel = new EventEmitter<boolean>();
 
-  enteredTitle = '';
-  enteredSummary ='';
-  enteredDate = '';
+  enteredTitle = signal('');
+  enteredSummary = signal('');
+  enteredDate = signal('');
 
-  onCancelTask(){
+  onCancelTask() {
     this.cancel.emit(false);
   }
 }
