5e8966c18b5ad1a615ff11b158c1c1060340f362
Akshay <akshaybawane93@gmail.com>
2024-09-03 20:07:11 +0530

    Two way binging

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,14 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
   @Output() cancel = new EventEmitter<boolean>();
+
+  enteredTitle = '';
+  enteredSummary ='';
+  enteredDate = '';
 
   onCancelTask(){
     this.cancel.emit(false);
