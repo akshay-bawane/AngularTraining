a4eaf9081c756e898af1627e7cb6fe1b04e09a88
Akshay <akshaybawane93@gmail.com>
2024-09-03 14:03:52 +0530

    Passing value to new component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,8 +11,7 @@ export class UserComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
 
-  //@Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
