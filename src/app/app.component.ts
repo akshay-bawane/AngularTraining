35786eb65eea2b82e3c710f5d407cdf2f39646dd
Akshay <akshaybawane93@gmail.com>
2024-09-03 14:50:47 +0530

    Reverting back to latest For and If

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
@@ -11,7 +10,7 @@ import { TasksComponent } from "./tasks/tasks.component";
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
