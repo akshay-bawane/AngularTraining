6a32feecad155d1fc08bc40710058c39da91d5d8
Akshay <akshaybawane93@gmail.com>
2024-09-03 16:22:18 +0530

    Creating tasks for users

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+interface Task{
+  id: string,
+  userId: string,
+  title: string,
+  summary: string,
+  dueDate: string
+}
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({required:true}) task!: Task;
+}
