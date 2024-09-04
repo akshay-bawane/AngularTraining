8673a732d7cf4b79ea8c63d00c51213251b375f6
Akshay <akshaybawane93@gmail.com>
2024-09-04 14:29:58 +0530

    Changing currency to INR

--- 02-investment-calculator/src/app/investment-results/investment-results.component.ts
+++ 02-investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
 import { type InvestmentOutput } from '../investment-output.model';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
