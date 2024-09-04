1cb3a328855383c2d7e8cdcf7e9a8d514a6312d9
Akshay <akshaybawane93@gmail.com>
2024-09-04 10:28:20 +0530

    Seperating Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { AddTaskComponent } from "./tasks/add-task/add-task.component";
 import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, AddTaskComponent],
+    declarations: [AppComponent, HeaderComponent, UserComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule, SharedModule]
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 
 export class AppModule { }
