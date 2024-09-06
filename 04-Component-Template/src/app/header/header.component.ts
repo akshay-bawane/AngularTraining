9f512e16c542353cd2053a8c01ae85e9c6a811b8
Akshay <akshaybawane93@gmail.com>
2024-09-06 15:28:23 +0530

    Attribute selector

--- 04-Component-Template/src/app/header/header.component.ts
+++ 04-Component-Template/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../shared/button/button.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
---
+++ 04-Component-Template/src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
