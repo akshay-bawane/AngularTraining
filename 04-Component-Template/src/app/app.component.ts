222baaa52ff2be86eb1d2b484310e02b6008e9a3
Akshay <akshaybawane93@gmail.com>
2024-09-06 12:15:12 +0530

    Dashboard item component are seperated

--- 04-Component-Template/src/app/app.component.ts
+++ 04-Component-Template/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
 })
 export class AppComponent {
   
