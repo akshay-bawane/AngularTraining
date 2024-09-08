1173ca70889c9300f2f7dbdf993dbdff33c6b181
Akshay <akshaybawane93@gmail.com>
2024-09-08 11:00:03 +0530

    Replacing wrapper with :host for css

--- 04-Component-Template/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ 04-Component-Template/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -5,7 +5,11 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  // host:{
+  //   class: 'dashboard-item'
+  // },
+  
 })
 export class DashboardItemComponent {
   @Input({ required: true }) image!: { src: string; alt: string };
