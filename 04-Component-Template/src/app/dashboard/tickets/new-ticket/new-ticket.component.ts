0fa2cb318549c683d38e131843531477cba8e75d
Akshay <akshaybawane93@gmail.com>
2024-09-08 10:43:50 +0530

    Styles and Style Encapsulation

--- 04-Component-Template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 04-Component-Template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
--- 04-Component-Template/src/app/shared/button/button.component.ts
+++ 04-Component-Template/src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
