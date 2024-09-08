8a51ea65ca0eab16914dd62a93ca419be72f62cf
Akshay <akshaybawane93@gmail.com>
2024-09-08 11:03:29 +0530

    HostBinding and Host

--- 04-Component-Template/src/app/shared/control/control.component.ts
+++ 04-Component-Template/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,10 +7,11 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  host: {
-    class: 'control'
-  }
+  // host: {
+  //   class: 'control'
+  // }
 })
 export class ControlComponent {
+  @HostBinding('class') className = 'control'
   label = input.required<string>();
 }
