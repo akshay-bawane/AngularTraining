08724ca81fa3b97a2797a15b477fc2e26914b76f
Akshay <akshaybawane93@gmail.com>
2024-09-10 21:21:20 +0530

    HTMLInputElement

--- 04-Component-Template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 04-Component-Template/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+  
+  onSubmit(titleElement: HTMLInputElement) {
+    console.dir(titleElement.value);
+  }
 }
